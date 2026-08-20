bca6a9724566918146efeb4a94f5fae6e069843f
Proddy <josh@zhbot.org>
2026-08-20 22:16:14 +0100

    refactor/Fix rune grid rendering + add total

--- src/main/java/org/zhbot/zmi_overlays/overlays/RuneGridComponent.java
+++ src/main/java/org/zhbot/zmi_overlays/overlays/RuneGridComponent.java
@@ -4,8 +4,10 @@ import lombok.Getter;
 import lombok.Setter;
 import net.runelite.client.game.ItemManager;
 import net.runelite.client.ui.overlay.components.LayoutableRenderableEntity;
+import net.runelite.client.ui.overlay.components.TextComponent;
 
 import java.awt.*;
+import java.text.NumberFormat;
 import java.util.Map;
 
 public class RuneGridComponent implements LayoutableRenderableEntity {
@@ -14,6 +16,8 @@ public class RuneGridComponent implements LayoutableRenderableEntity {
 
     public static final int PADDING_X = 4;
     public static final int PADDING_Y = 2;
+
+    private static final AlphaComposite DISABLED_COMPOSITE = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.4f);
 
     @Getter
     @Setter
@@ -34,30 +38,19 @@ public class RuneGridComponent implements LayoutableRenderableEntity {
         this.itemManager = itemManager;
         this.runes = runes;
 
-        var totalItems = runes.size();
-        var columns = Math.min(totalItems, ICONS_PER_ROW);
-        var rows = (int)Math.ceil((double)totalItems / ICONS_PER_ROW);
-
-        var totalWidth = columns * ICON_SIZE + PADDING_X * 2;
-        var totalHeight = rows * ICON_SIZE + PADDING_Y * 2;
-
-        this.setPreferredSize(new Dimension(totalWidth, totalHeight));
+        this.setPreferredSize(getGridSize());
     }
 
     @Override
-    public Dimension render(Graphics2D graphics) {
+    public Dimension render(Graphics2D graphics)
+    {
+        var renderDimension = getGridSize();
+
         if (runes.isEmpty())
         {
-            bounds = new Rectangle(preferredLocation, new Dimension(0, 0));
-            return new Dimension(0, 0);
+            bounds = new Rectangle(preferredLocation, renderDimension);
+            return renderDimension;
         }
-
-        var totalItems = runes.size();
-        var columns = Math.min(totalItems, ICONS_PER_ROW);
-        var rows = (int)Math.ceil((double)totalItems / ICONS_PER_ROW);
-
-        var totalWidth = columns * ICON_SIZE;
-        var totalHeight = rows * ICON_SIZE;
 
         var x = 0;
         var y = 0;
@@ -71,12 +64,13 @@ public class RuneGridComponent implements LayoutableRenderableEntity {
             var showQuantity = amount > 0;
             var img = itemManager.getImage(itemId, amount, showQuantity);
 
-            if (showQuantity)
-                graphics.setComposite(composite);
-            else
-                graphics.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.4f));
+            if (!showQuantity)
+                graphics.setComposite(DISABLED_COMPOSITE);
 
             graphics.drawImage(img, preferredLocation.x + x + PADDING_X, preferredLocation.y + y + PADDING_Y, null);
+
+            if (!showQuantity)
+                graphics.setComposite(composite);
 
             x += ICON_SIZE;
 
@@ -86,10 +80,40 @@ public class RuneGridComponent implements LayoutableRenderableEntity {
                 y += ICON_SIZE;
             }
         }
-        graphics.setComposite(composite);
 
-        Dimension renderDimension = new Dimension(totalWidth, totalHeight);
+        var total = runes.values().stream().mapToInt(Integer::intValue).sum();
+
+        var totalX = preferredLocation.x + x + PADDING_X;
+        var totalY = preferredLocation.y + y + PADDING_Y;
+
+        var text = new TextComponent();
+        text.setColor(Color.WHITE);
+
+        text.setText("Total:");
+        text.setPosition(totalX, totalY + 16);
+        text.render(graphics);
+
+        text.setText(NumberFormat.getNumberInstance().format(total));
+        text.setPosition(totalX, totalY + 29);
+        text.render(graphics);
+
         bounds = new Rectangle(preferredLocation, renderDimension);
         return renderDimension;
+    }
+
+    private Dimension getGridSize()
+    {
+        var totalItems = runes.size();
+
+        if (totalItems == 0)
+            return new Dimension();
+
+        var columns = Math.min(totalItems, ICONS_PER_ROW);
+        var rows = (totalItems + ICONS_PER_ROW - 1) / ICONS_PER_ROW;
+
+        return new Dimension(
+                columns * ICON_SIZE,
+                rows * ICON_SIZE
+        );
     }
 }
